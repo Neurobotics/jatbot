c95dcdb30ae379276f6da0339a397f400458ccda
Dmitry Konyshev <d.konyshev@neurobotics.ru>
2021-12-15 16:27:43 +0300

    Add mitt

--- quasar.conf.js
+++ quasar.conf.js
@@ -24,7 +24,8 @@ module.exports = configure(function (ctx) {
     boot: [
       'i18n',
       'axios',
-      'jat'
+      'jat',
+      'mitt'
     ],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
